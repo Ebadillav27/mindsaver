9f8992854dd86c6621deff626b429745919d0c92
Ebadillav27 <erixxer@gmail.com>
2018-08-24 00:35:42 -0600

    se me había olvidado guardar main

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-'''
-reunir librerias de windows, buscar la linea de prompt para bloquear la sesion
-y buscar la manera de reiniciar el temporizador cada vez que la sesión se reinicie.
-'''
+import os
+import PySimpleGUI as sg 
+import time 
+
+#cmd: rundll32.exe user32.dll, LockWorkStation
+
+starter = 0 
+max_time = 25 
+
+os.system("rundll32.exe user32.dll, LockWorkStation")
